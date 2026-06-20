d901df64f6e8aa83fe95a0a1c2bc62c7fca00c58
PepiMartin28 <martinpepi_99@hotmail.com>
2026-06-20 00:01:54 -0300

    Refactor geolocation staging model to consolidate zip code data from geolocation, sellers, and customers

--- src/dbt/models/staging/stg_geolocation.sql
+++ src/dbt/models/staging/stg_geolocation.sql
@@ -1,7 +1,36 @@
-select
-    geolocationZipCodePrefix,
-    geolocationLatitude,
-    geolocationLongitude,
-    geolocationCityName,
-    geolocationState
-from {{ source('olist_silver', 'geolocation_silver') }} 
+with all_zipCodes as (
+    select
+        geolocationZipCodePrefix as zipCodePrefix,
+        geolocationCityName as city,
+        geolocationState as stateName
+    from {{ source('olist_silver', 'geolocation_silver') }} 
+    
+    union
+    
+    select
+        sellerZipCodePrefix as zipCodePrefix,
+        sellerCity as city,
+        sellerState as stateName
+    from {{ source('olist_silver', 'sellers_silver') }}
+    
+    union
+
+    select
+        customerZipCodePrefix as zipCodePrefix,
+        customerCity as city,
+        customerState as stateName
+    from {{ source('olist_silver', 'customers_silver') }}
+)
+
+select distinct
+    zip.zipCodePrefix as geolocationZipCodePrefix,
+    geo.geolocationLatitude as geolocationLatitude,
+    geo.geolocationLongitude as geolocationLongitude,
+    zip.city as geolocationCityName,
+    zip.stateName as geolocationState
+from all_zipCodes zip
+left join {{ source('olist_silver', 'geolocation_silver') }} geo
+    on 
+        zip.zipCodePrefix = geo.geolocationZipCodePrefix 
+        and zip.city = geo.geolocationCityName 
+        and zip.stateName = geo.geolocationState
